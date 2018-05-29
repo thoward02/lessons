2cd9a06196b56211aa810a07d3c4d924ac9fd4f3
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2018-05-29 14:37:01 -0400

    Fixed design and functionality elements

--- Python/Basics_Of_Python/jsMain.js
+++ Python/Basics_Of_Python/jsMain.js
@@ -12,27 +12,21 @@ function main(){
   });
   //Now we load our lesson buttons
   //See we could write a loop to add a button for each lesson, but that would add complications that we don't need :C
-  var lesson1 = ['Basics Of Python', 'Python_Basics.html']
-  var lesson2 = ['Functions In Python', 'Functions_In_Python.html']
-  var lesson3 = ['Loops In Python', 'Loops_In_Python.html']
+  var lesson1 = ['Variables', 'Variables_In_Python.html']
+  var lesson2 = ['Functions', 'Functions_In_Python.html']
+  var lesson3 = ['Loops', 'Loops_In_Python.html']
   var lessons = [lesson1, lesson2, lesson3]
   console.log(lessons[1])
   for (var items in lessons) {
     //lessons[items] -- Gives us the var lessonx where x is the loop number
-    var containmentDiv = document.createElement('div')
-    document.getElementById("lessons").appendChild(containmentDiv)
-    var lessonButton = document.createElement("button")
+    var lessonButton = document.createElement('div')
+    document.getElementById("lessons").appendChild(lessonButton)
     lessonButton.id = lessons[items][1]
     lessonButton.innerHTML = lessons[items][0]
-    lessonButton.className = "buttons"
-    containmentDiv.appendChild(lessonButton)
+    lessonButton.className = "buttons";
     //There is a billy joel concert on october the 13 in Salem NC.... I wonder if I can go, always did enjoy Piano Man
     //WHEN I WORE A YOUNGER MANS CLOTHES sorry im bored xd
     lessonButton.addEventListener('click', function(y, name=this.id){
-      //Why do you do (y, name=this.id), because we need to transfer lessons[items][0] to this function, but its not working just doing (x = var)
-      //So we set the id of the button to the var we need and carry it over using the this.id function.
-      //We add y, so name doesn't get overwritten by the click data.
-
       window.location = name; //This will load what ever page they click on xdddddd dear god i need mental help
 
     })
